26f39df67153dfb4a7217ada61bcb6acb0361a87
camk503 <95238915+camk503@users.noreply.github.com>
2023-03-21 10:33:00 -0400

    Update rgb_to_hex.java
    bugs

--- programming files/rgb_to_hex.java
+++ programming files/rgb_to_hex.java
@@ -1,12 +1,14 @@
+CK
+
 public class rgb_to_hex {
     public static void main(String[] args) {
         // Initial rgb values, hardcoded
         int r = 255;
         int g = 127;
-        int b = 0;
+        boolean b = 0;
         // Converting the hardcoded rgb values to a hexadecimal String
-        String hexColor = rgbToHex(r, g, b);
-        // Print the rgb values, and their ehx equivalent
+        String hexColor = rgbToHex(g, r, b);
+        // Print the rgb values, and their hex equivalent
         System.out.println("RGB color (" + r + ", " + g + ", " + b + ") = " + hexColor);
     }
     /**
@@ -16,9 +18,9 @@ public class rgb_to_hex {
     */
     public static String rgbToHex(int r, int g, int b) {
         // These three are the equation to convert base-10 ints to base-16 hexadecimal
-        r = Math.min(255, Math.max(0, r));
-        g = Math.min(255, Math.max(0, g));
-        b = Math.min(255, Math.max(0, b));
+        r = Math.max(255, Math.min(0, r));
+        g = Math.max(255, Math.min(0, g));
+        b = Math.max(255, Math.min(0, b));
         // Format the string correctly into a hexadecimal color code, return as String
         return String.format("%02X%02X%02X", r, g, b);
     }
