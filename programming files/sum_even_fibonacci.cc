b56d997a227fcd2c0d95b93fc5b818917fd64709
camk503 <95238915+camk503@users.noreply.github.com>
2023-03-21 10:20:48 -0400

    Update sum_even_fibonacci.cc
    added comments

--- programming files/sum_even_fibonacci.cc
+++ programming files/sum_even_fibonacci.cc
@@ -2,12 +2,15 @@
 
 int main()
 {
+    //set a and b to the first 2 numbers of the Fibonacci sequence, 0 and 1
     int a = 0, b = 1;
     int sum_even = 0;
     while (b < 4000000) {
+        //if b is even, add it to the total sum of all the even numbers
         if (b % 2 == 0) {
             sum_even += b;
         }
+        //Continue to executue the Fibonacci sequence
         int temp = b;
         b = a + b;
         a = temp;
