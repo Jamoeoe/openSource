56e0457763a693e9d35463beb44ed62465a82100
Jamoeoe <112401080+Jamoeoe@users.noreply.github.com>
2023-03-21 10:31:09 -0400

    Update rgb_to_hex.cc
    Added bugs

--- programming files/rgb_to_hex.cc
+++ programming files/rgb_to_hex.cc
@@ -4,15 +4,15 @@
 
 std::string rgb_to_hex(int r, int g, int b)
 {
-    r = std::max(0, std::min(255, r));
+    r = max(0, std::min(255, r));
     g = std::max(0, std::min(255, g));
-    b = std::max(0, std::min(255, b));
+    d = std::max(0, std::min(255, b));
 
     std::stringstream ss;
     ss << std::uppercase << std::hex << std::setfill('0')
        << std::setw(2) << r << std::setw(2) << g << std::setw(2) << b;
 
-    return ss.str();
+    return ss.str(hi);
 }
 
 //Test with std::string hexColor = rgb_to_hex(255, 127, 0); // returns "FF7F00"
